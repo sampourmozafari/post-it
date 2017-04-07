8cfb0daf49f49c5bcb1fdc6be2fdc33c8d329ed2
Sam <sam@Sams-MacBook-Pro.local>
2017-04-07 11:19:17 +0200

    Bla

--- opslagstavle.php
+++ opslagstavle.php
@@ -11,7 +11,13 @@
   </head>
   <body>
     <?php
-    echo $stmt;
+if( $_POST["name"] || $_POST["email"] || $_POST["contact"])
+{
+echo "Welcome: ". $_POST['name']. "<br />";
+echo "Your Email is: ". $_POST["email"]. "<br />";
+echo "Your Mobile No. is: ". $_POST["contact"];
+}
+?>
     ?>
   </body>
 </html>
